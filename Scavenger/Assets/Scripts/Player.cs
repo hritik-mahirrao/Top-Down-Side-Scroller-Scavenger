db8d7a6ce43e0777e6ed6a06a26732cd290e565a
Hritik Mahirrao <hritiknarendra@gmail.com>
2018-08-16 11:13:29 +0530

    Revert "Before Separating RayCast stuff"
    This reverts commit 1683e32c401e84b2be70f0c8578ffec7ab6c9adf.

--- Scavenger/Assets/Scripts/Player.cs
+++ Scavenger/Assets/Scripts/Player.cs
@@ -5,47 +5,23 @@ using UnityEngine;
 [RequireComponent(typeof(Controller2D))]
 public class Player : MonoBehaviour
 {
-    public float jumpHeight;
-    public float timeToJumpApex;
-    public float accelerationTimeAirborne = 0.2f;
-    public float accelerationTimeGrounded = 0.1f;
+
     public float moveSpeed = 6;
-    float gravity;
-    float jumpVelocity;
+    float gravity = -20;
     Vector2 velocity;
-    public float velocityXSmoothing;
 
     Controller2D controller;
 
     void Start()
     {
         controller = GetComponent<Controller2D>();
-
-        //gravity = -(2 * jumpHeight) / Mathf.Pow(timeToJumpApex, 2);
-        //jumpVelocity = Mathf.Abs(gravity) * timeToJumpApex;
     }
 
     void Update()
     {
-
-        gravity = -(2 * jumpHeight) / Mathf.Pow(timeToJumpApex, 2);
-        jumpVelocity = Mathf.Abs(gravity) * timeToJumpApex;
-
-        if (controller.collisions.above || controller.collisions.below)
-        {
-            velocity.y = 0;
-        }
-
         Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
 
-        if (Input.GetKeyDown(KeyCode.Space) && controller.collisions.below)
-        {
-            velocity.y += 10;
-        }
-
-        float targetVelocityX = input.x * moveSpeed;
-        velocity.x = Mathf.SmoothDamp(velocity.x, targetVelocityX, ref velocityXSmoothing, (controller.collisions.below) ? accelerationTimeGrounded : accelerationTimeAirborne);
-
+        velocity.x = input.x * moveSpeed;
         velocity.y += gravity * Time.deltaTime;
         controller.Move(velocity * Time.deltaTime);
     }
