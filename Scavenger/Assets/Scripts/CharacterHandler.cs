2018c5d7346094704714ef73918ec8f8f92c5b6c
Hritik Mahirrao <hritiknarendra@gmail.com>
2018-08-23 21:12:15 +0530

    Camera drag implemented

---
+++ Scavenger/Assets/Scripts/CharacterHandler.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterHandler : MonoBehaviour
+{
+    public enum CharacterWeaponary
+    {
+        Holster,
+        Pistol
+    };
+
+    public CharacterWeaponary characterWeaponary;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DetermineWeapon();
+    }
+
+    public void DetermineWeapon()
+    {
+        switch (characterWeaponary)
+        {
+            case CharacterWeaponary.Pistol:
+                break;
+        }
+    }
+}
--- Scavenger/Assets/Scripts/DeveloperWorkBench.cs
+++ Scavenger/Assets/Scripts/DeveloperWorkBench.cs
@@ -13,15 +13,31 @@ public class DeveloperWorkBench : MonoBehaviour
 
     public GameMode gameMode;
 
-    // Use this for initialization
+    private float dist;
+    private Vector3 MouseStart;
+    private Vector3 derp;
+
     void Start()
     {
-
+        dist = transform.position.z;  // Distance camera is above map
     }
 
-    // Update is called once per frame
     void Update()
     {
+        if (Input.GetMouseButtonDown(2))
+        {
+            MouseStart = new Vector3(Input.mousePosition.x, Input.mousePosition.y, dist);
+            MouseStart = Camera.main.ScreenToWorldPoint(MouseStart);
+            MouseStart.z = transform.position.z;
 
+        }
+        else if (Input.GetMouseButton(2))
+        {
+            var MouseMove = new Vector3(Input.mousePosition.x, Input.mousePosition.y, dist);
+            MouseMove = Camera.main.ScreenToWorldPoint(MouseMove);
+            MouseMove.z = transform.position.z;
+            transform.position = transform.position - (MouseMove - MouseStart);
+        }
     }
+
 }
